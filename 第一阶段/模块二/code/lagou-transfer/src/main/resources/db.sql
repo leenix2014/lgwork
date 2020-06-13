140c0861c846e5a3d357037beb833196c6b350c4
李全林 <zs08378022@126.com>
2020-06-13 20:34:48 +0800

    视频讲解

---
+++ 第一阶段/模块二/code/lagou-transfer/src/main/resources/db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `account` (
+  `cardNo` varchar(256) DEFAULT NULL,
+  `name` varchar(256) DEFAULT NULL,
+  `money` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
