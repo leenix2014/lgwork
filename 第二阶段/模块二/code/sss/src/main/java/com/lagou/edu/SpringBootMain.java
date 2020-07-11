978598866e3451c5151cc582ea571984897c560c
李全林 <zs08378022@126.com>
2020-07-11 18:14:51 +0800

    spring session集成

--- 第二阶段/模块二/code/sss/src/main/java/com/lagou/edu/SpringBootMain.java
+++ 第二阶段/模块二/code/sss/src/main/java/com/lagou/edu/SpringBootMain.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-//@EnableWebMvc
+@EnableRedisHttpSession
 public class SpringBootMain extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
